59794a01ca779f10729e61dae43dd2edf4037d3f
Arthur Assis <arthur120496@gmail.com>
2017-09-22 01:34:37 -0300

    Applies Commentaries technique at file timer.cpp

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,13 +1,28 @@
+/*
+ *  File: timer.cpp
+ *
+ *  Description:  Implements Timer class
+ */
+
 #include <timer.hpp>
 
-Timer::Timer() {}
+// No params constructor method for Timer
+Timer::Timer() {
 
+}
+
+// Adds time to Timer's time
+// Recivies the param dTime,as a float, that represents time that will bee add
 void Timer::Update(float dTime) {
-	time+=dTime;
+	time += dTime;
 }
+
+// Resets the Timer's time
 void Timer::Restart() {
-	time=0;
+	time = 0;
 }
+
+// Returns the Timer's time
 float Timer::Get() {
 	return time;
 }
