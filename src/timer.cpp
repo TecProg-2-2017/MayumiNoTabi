74ad82efb0e476253d3fb10e0a85cadd67c26edb
Arthur Assis <arthur120496@gmail.com>
2017-09-24 17:35:13 -0300

    Refactories commentaries technique at file timer.cpp

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,28 +1,50 @@
-/*
- *  File: timer.cpp
+ /*!
+ *  \file File timer.cpp
+ *  \brief Implementation of the class Timer
  *
- *  Description:  Implements Timer class
+ *  Auxiliary documentation
+ *  \sa timer.hpp
  */
-
 #include <timer.hpp>
 
-// No params constructor method for Timer
+/*!
+	@class Timer
+	@brief This class provides the timer to the game
+*/
+
+//! A constructor.
+    /*!
+    This is a empty constructor method of Timer class
+    */
 Timer::Timer() {
 
 }
 
-// Adds time to Timer's time
-// Recivies the param dTime,as a float, that represents time that will bee add
+/*!
+	@fn void Timer::Update(float dTime)
+	@brief Method that adds time to the timer
+	@param dTime
+	@brief A float, that represents time that will bee add to the timer
+	@return The execution of this method returns no value
+*/
 void Timer::Update(float dTime) {
 	time += dTime;
 }
 
-// Resets the Timer's time
+/*!
+	@fn void Timer::Restart()
+	@brief Method that resets the timer
+	@return The execution of this method returns no value
+*/
 void Timer::Restart() {
 	time = 0;
 }
 
-// Returns the Timer's time
+/*!
+	@fn float Timer::Get()
+	@brief A getter of the attribute time
+	@return A positive float, that represents the timer's current time
+*/
 float Timer::Get() {
 	return time;
 }
