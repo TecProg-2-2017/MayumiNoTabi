3bed036f0dbf1a1a0732f2fd8c4496066d7fffe3
matheusbsilva <matheusbattista@hotmail.com>
2017-10-29 14:20:10 -0200

    Apply logging on inputManager.cpp

--- src/inputManager.cpp
+++ src/inputManager.cpp
@@ -13,7 +13,8 @@
  *  @return An InputManager object
  */
 InputManager::InputManager() {
-
+    LOG_MSG("InputManager constructor");
+    LOG_METHOD_CLOSE("InputManager::InputManager()","InputManager");
 }
 
 /*!
@@ -21,7 +22,8 @@ InputManager::InputManager() {
  *  @brief Destructor method of Input Manager 
  */
 InputManager::~InputManager() {
-
+    LOG_MSG("InputManager destructor");
+    LOG_METHOD_CLOSE("InputManager::~InputManager()","void");
 }
 
 /*!
@@ -31,16 +33,20 @@ InputManager::~InputManager() {
  *  @return The method returns no param
  */
 void InputManager::update_mouse_button_state(SDL_Event event) {
+    LOG_METHOD_START("InputManager::update_mouse_button_state");
+
     bool mouse_state = true; //! <Mouse button state
   
     assert(event != NULL); 
 
     if (event.type==SDL_MOUSEBUTTONDOWN) {
         mouse_state = true;
+        LOG_VARIABLE("mouse_state","true");
     }
 
     else if (event.type==SDL_MOUSEBUTTONUP) {
         mouse_state = false;
+        LOG_VARIABLE("mouse_state","false");
     }
 
     if (/*event.button.button>=0 && */event.button.button<6 && mouse_current_state[event.button.button]!=mouse_state) {
@@ -51,6 +57,8 @@ void InputManager::update_mouse_button_state(SDL_Event event) {
         // Do nothing
     }
 
+    LOG_METHOD_CLOSE("InputManager::update_mouse_button_state","void");
+
 }
 
 /*!
@@ -60,15 +68,21 @@ void InputManager::update_mouse_button_state(SDL_Event event) {
  *  @return The method returns no param
  */
 void InputManager::update_key_button_state(SDL_Event event) {
+    LOG_METHOD_START("InputManager::update_key_button_state");
+
     bool key_state = true; //! <Key button state
 
     assert(event != NULL); 
 
     if (event.type == SDL_KEYDOWN) {
         key_state = true;
+
+        LOG_VARIABLE("key_state","true");
     }
     else if (event.type == SDL_KEYUP) {
         key_state = false;
+
+        LOG_VARIABLE("key_state","false");
     }
     else {
         // Do nothing 
@@ -76,6 +90,8 @@ void InputManager::update_key_button_state(SDL_Event event) {
 
     key_current_state[event.key.keysym.sym]=key_state;
     key_updated_state[event.key.keysym.sym]=update_counter;
+
+    LOG_METHOD_CLOSE("InputManager::update_key_button_state","void");
 }
 
 /*!
@@ -85,13 +101,18 @@ void InputManager::update_key_button_state(SDL_Event event) {
  *  @return The method returns no param
  */
 void InputManager::insert_text(SDL_Event event) {
+    LOG_METHOD_START("InputManager::insert_text");
+    
     assert(event != NULL); 
 
     string input(event.text.text);
+    LOG_VARIABLE("input", input);
     assert(input != NULL); 
 
     text->insert(text_cursor,input);
     text_cursor += input.size();
+
+    LOG_METHOD_CLOSE("InputManager::insert_text","void");
 }
 
 /*!
@@ -101,6 +122,8 @@ void InputManager::insert_text(SDL_Event event) {
  *  @return The method returns no param
  */
 void InputManager::erase_text(SDL_Event event) {
+    LOG_METHOD_START("InputManager::erase_text");
+
     assert(event != NULL); 
 
     //! If event key is backspace and text is not empty
@@ -108,6 +131,7 @@ void InputManager::erase_text(SDL_Event event) {
             text->size() && text_cursor) {
 
         text->erase(--text_cursor,1);
+        LOG_MSG("Text erased");
 
         if (text_cursor>text->size()) {
             text_cursor=text->size();
@@ -120,6 +144,7 @@ void InputManager::erase_text(SDL_Event event) {
     else {
         return;
     }
+    LOG_METHOD_CLOSE("InputManager::erase_text","void");
 }
 
 /*!
@@ -130,14 +155,22 @@ void InputManager::erase_text(SDL_Event event) {
  *  @warning Method maybe need refactoring
  */
 void InputManager::input_event_handler(float time) {
+    LOG_METHOD_START("InputManager::input_event_handler");
+
     int x_position = 0; //! <Horizontal axis position
     int y_position = 0; //! <Vertical axis position
 
     //! Get mouse position 
     SDL_GetMouseState(&x_position,&y_position);
+    LOG_VARIABLE("x_position",x_position);
+    LOG_VARIABLE("y_position",y_position);
+
     mouse_motion = (mouse_position.x!=x_position || mouse_position.y!=y_position);
+
     mouse_position.x = (float)x_position;
     mouse_position.y = (float)y_position;
+    LOG_VARIABLE("mouse_position.x",mouse_position.x);
+    LOG_VARIABLE("mouse_position.y",mouse_position.y);
     
     quit_requested=false;
     
@@ -151,16 +184,20 @@ void InputManager::input_event_handler(float time) {
 
         //! Event is quit
         if (event.type==SDL_QUIT) {
+            LOG_MSG("Quit game event");
             quit_requested=true;
+            LOG_VARIABLE("quit_requested",quit_requested);
         }
 
         //! Event is Mouse button down
         else if (event.type==SDL_MOUSEBUTTONDOWN || event.type==SDL_MOUSEBUTTONUP) {
+            LOG_MSG("Mouse button down or up event");
             update_mouse_button_state(event);
         }
     
         //! Event is Key down 
         else if (event.type==SDL_KEYDOWN) {
+            LOG_MSG("Key down event");
 
             if (!event.key.repeat) {
                 update_key_button_state(event);
@@ -173,6 +210,7 @@ void InputManager::input_event_handler(float time) {
 
         //! Event is Key up 
         else if (event.type==SDL_KEYUP) {
+            LOG_MSG("Key up event");
             update_key_button_state(event);
         }
         
@@ -181,11 +219,13 @@ void InputManager::input_event_handler(float time) {
 
             //! Event is text input
             if (event.type==SDL_TEXTINPUT) {
+                LOG_MSG("Text input event");
                 insert_text(event);
             }
 
-           //! Event is key down when inserting text 
+           //! Event is key down when text is not empty 
             else if (event.type==SDL_KEYDOWN) {
+                LOG_MSG("Key down event when handling text");
                 text_cursor_blinker.Restart();
 
                 erase_text(event);
@@ -193,11 +233,13 @@ void InputManager::input_event_handler(float time) {
 
                 //! Move cursor to the left if left key is pressed 
                 if (event.key.keysym.sym == SDLK_LEFT && text_cursor > 0) {
+                    LOG_MSG("Move text cursor to the left");
                     text_cursor--;
                 }
 
                 //! Move cursor to the right if right key is pressed 
                 else if (event.key.keysym.sym == SDLK_RIGHT && text_cursor < text->size()) {
+                    LOG_MSG("Move text cursor to the right");
                     text_cursor++;
                 }
             }//! <End of if which event is key down
@@ -207,6 +249,8 @@ void InputManager::input_event_handler(float time) {
         }
     } //! <End of while which iteration through sdl events
     update_counter++;
+
+    LOG_METHOD_CLOSE("InputManager::input_event_handler","void");
 }
 
 /*!
@@ -216,7 +260,10 @@ void InputManager::input_event_handler(float time) {
  *  @return True of False 
  *  @warning Simplify return 
  */
-bool InputManager::key_preessed(int key) {
+bool InputManager::key_pressed(int key) {
+    LOG_METHOD_START("InputManager::key_pressed");
+    LOG_METHOD_CLOSE("InputManager::key_pressed","bool");
+
     return (key_current_state[key] && key_updated_state[key]==update_counter-1);
 }
 
@@ -228,6 +275,9 @@ bool InputManager::key_preessed(int key) {
  *  @warning Simplify return 
  */
 bool InputManager::key_released(int key) {
+    LOG_METHOD_START("InputManager::key_released");
+    LOG_METHOD_CLOSE("InputManager::key_released","bool");
+
     return ((!key_current_state[key]) && key_updated_state[key]==update_counter-1);
 }
 
@@ -238,6 +288,9 @@ bool InputManager::key_released(int key) {
  *  @return True of False 
  */
 bool InputManager::key_is_down(int key) {
+    LOG_METHOD_START("InputManager::key_is_down");
+    LOG_METHOD_CLOSE("InputManager::key_is_down","bool");
+
     return (key_current_state[key]);
 }
 
@@ -249,6 +302,9 @@ bool InputManager::key_is_down(int key) {
  *  @warning Simplify return 
  */
 bool InputManager::mouse_button_pressed(int button) {
+    LOG_METHOD_START("InputManager::mouse_button_pressed");
+    LOG_METHOD_CLOSE("InputManager::mouse_button_pressed","bool");
+
     return (mouse_current_state[button] && mouse_updated_state[button]==update_counter-1);
 }
 
@@ -260,6 +316,9 @@ bool InputManager::mouse_button_pressed(int button) {
  *  @warning Simplify return 
  */
 bool InputManager::mouse_button_released(int button) {
+    LOG_METHOD_START("InputManager::mouse_button_released");
+    LOG_METHOD_CLOSE("InputManager::mouse_button_released","bool");
+
     return ((!mouse_current_state[button]) && mouse_updated_state[button]==update_counter-1);
 }
 
@@ -270,6 +329,9 @@ bool InputManager::mouse_button_released(int button) {
  *  @return True of False 
  */
 bool InputManager::mouse_button_is_down(int button) {
+    LOG_METHOD_START("InputManager::mouse_button_is_down");
+    LOG_METHOD_CLOSE("InputManager::mouse_button_is_down","bool");
+
     return (mouse_current_state[button]);
 }
 
@@ -279,6 +341,9 @@ bool InputManager::mouse_button_is_down(int button) {
  *  @return True of False 
  */
 bool InputManager::mouse_is_moving() {
+    LOG_METHOD_START("InputManager::mouse_is_moving");
+    LOG_METHOD_CLOSE("InputManager::mouse_is_moving","bool");
+
     return mouse_is_moving;
 }
 
@@ -288,6 +353,9 @@ bool InputManager::mouse_is_moving() {
  *  @return Vec2 
  */
 Vec2 InputManager::get_mouse_position() {
+    LOG_METHOD_START("InputManager::get_mouse_position");
+    LOG_METHOD_CLOSE("InputManager::get_mouse_position","Vec2");
+
     return mouse_position;
 }
 
@@ -297,6 +365,9 @@ Vec2 InputManager::get_mouse_position() {
  *  @return integer 
  */
 int InputManager::get_mouse_x_position() {
+    LOG_METHOD_START("InputManager::get_mouse_x_position");
+    LOG_METHOD_CLOSE("InputManager::get_mouse_x_position","int");
+
     return mouse_position.x;
 }
 
@@ -306,6 +377,9 @@ int InputManager::get_mouse_x_position() {
  *  @return integer 
  */
 int InputManager::get_mouse_y_position() {
+    LOG_METHOD_START("InputManager::get_mouse_y_position");
+    LOG_METHOD_CLOSE("InputManager::get_mouse_y_position","int");
+
     return mouse_position.y;
 }
 
@@ -316,6 +390,7 @@ int InputManager::get_mouse_y_position() {
  *  @return The method returns no param
  */
 void InputManager::start_text_input(string* t) {
+    LOG_METHOD_START("InputManager::start_text_input");
 
     //! If param is empty, returns
     if (t == nullptr) { 
@@ -329,6 +404,8 @@ void InputManager::start_text_input(string* t) {
     text = t;
     text_cursor = text->size();
     text_cursor_blinker.restart_time();
+
+    LOG_METHOD_CLOSE("InputManager::start_text_input","void");
 }
 
 /*!
@@ -338,6 +415,7 @@ void InputManager::start_text_input(string* t) {
  *  @return The method returns no param
  */
 void InputManager::stop_text_input(string* t) {
+    LOG_METHOD_START("InputManager::stop_text_input");
     
     //! If class attribute text is different from param, returns
     if (text != t) {
@@ -349,6 +427,8 @@ void InputManager::stop_text_input(string* t) {
     
     text = nullptr;
     SDL_StopTextInput();
+
+    LOG_METHOD_CLOSE("InputManager::stop_text_input","void");
 }
 
 /*!
@@ -357,6 +437,9 @@ void InputManager::stop_text_input(string* t) {
  *  @return unsigned integer 
  */
 uint InputManager::get_text_cursor_position() {
+    LOG_METHOD_START("InputManager::get_text_cursor_position");
+    LOG_METHOD_CLOSE("InputManager::get_text_cursor_position","uint");
+
     return text_cursor;
 }
 
@@ -367,6 +450,9 @@ uint InputManager::get_text_cursor_position() {
  *  @warning Simplify method return
  */
 bool InputManager::text_cursor_blink() {
+    LOG_METHOD_START("InputManager::text_cursor_blink");
+    LOG_METHOD_CLOSE("InputManager::text_cursor_blink","bool");
+
     return !((int)(text_cursor_blinker.get_time()/0.5)%2);
 }
 
@@ -376,6 +462,9 @@ bool InputManager::text_cursor_blink() {
  *  @return True or false 
  */
 bool InputManager::get_quit_requested() {
+    LOG_METHOD_START("InputManager::get_quit_requested");
+    LOG_METHOD_CLOSE("InputManager::get_quit_requested","bool");
+
     return quit_requested;
 }
 
@@ -385,7 +474,10 @@ bool InputManager::get_quit_requested() {
  *  @return InputManager 
  */
 InputManager& InputManager::get_input_manager_instance() {
+    LOG_METHOD_START("InputManager& InputManager::get_input_manager_instance");
+
     static InputManager uniqueInst;
 
+    LOG_METHOD_CLOSE("InputManager& InputManager::get_input_manager_instance", "InputManager");
     return uniqueInst;
 }
