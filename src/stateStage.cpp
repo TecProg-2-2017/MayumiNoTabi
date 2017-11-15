e4ef1f259e8f560913f778e4c3bfb0c05bd7191a
Arthur Assis <arthur120496@gmail.com>
2017-11-15 15:00:16 -0200

    fixing stateStage

--- src/stateStage.cpp
+++ src/stateStage.cpp
@@ -48,10 +48,10 @@ StateStage::~StateStage() {}
 void StateStage::Begin() {
 
 	//! Loads the level and the player
-	level.Load(levelName);
+	level.load_level_from_file(levelName);
 	player = GameObject::MakePlayer(Vec2{550.0f,430.0f});
 	AddObject(player);
-	Camera::Follow(player);
+	Camera::follow(player);
 	level.load_level_objects();
 }
 
@@ -62,15 +62,15 @@ void StateStage::Begin() {
 	@brief a positive float, the represents the game time
 	@return The execution of this method returns no value
 */
-void StateStage::Update(float time) {
-	Camera::update(time);
+void StateStage::update(float time) {
+	Camera::update_camera(time);
 	//! Checks if the player requested to quit
-	if (INPUT.QuitRequested()){
-		quitRequested = true;
+	if (INPUT.get_quit_requested()){
+		quit_requested = true;
 	}
 
 	//! Checks if the player pressed the key 'ESC'
-	if (INPUT.KeyPress(KEY_ESC)){
+	if (INPUT.key_pressed(KEY_ESC)){
 		popRequested=true;
 	}
 
@@ -78,37 +78,37 @@ void StateStage::Update(float time) {
 	Vec2 pos = GO(PLAYER_UID)->pos;//!< A Vec2, that represents the player position
 
 	//! Checks if the player pressed the key 'u'
-	if (INPUT.KeyPress(KEY(u))){
+	if (INPUT.key_pressed(KEY(u))){
 		AddObject(GameObject::MakePorco(pos+Vec2{2000.0f,-100.0f}));
 	}
 
 	//! Checks if the player pressed the key 'p'
-	if (INPUT.KeyPress(KEY(p))){
+	if (INPUT.key_pressed(KEY(p))){
 		AddObject(GameObject::MakeMike(pos+Vec2{850.0f,-100.0f}));
 	}
 
 	//! Checks if the player pressed the key 'o'
-	if (INPUT.KeyPress(KEY(o))){
+	if (INPUT.key_pressed(KEY(o))){
 		AddObject(GameObject::MakeBanshee(pos+Vec2{850.0f,-100.0f},pos+Vec2{230.0f,-200.0f}));
 	}
 
 	//! Checks if the player pressed the key 'i'
-	if (INPUT.KeyPress(KEY(i))){
+	if (INPUT.key_pressed(KEY(i))){
 		AddObject(GameObject::MakeMask(pos+Vec2{850.0f,-100.0f}));
 	}
 
 	//! Checks if the player pressed the key 'y'
-	if (INPUT.KeyPress(KEY(y))){
+	if (INPUT.key_pressed(KEY(y))){
 		GO(player)->dead=true;
 	}
 
 	//! Checks if the player pressed the key 'n'
-	if (INPUT.KeyPress(KEY(n))){
+	if (INPUT.key_pressed(KEY(n))){
 		SETTINGS.showHP = !SETTINGS.showHP;
 	}
 
 	//! Checks if the player pressed the key 'm'
-	if (INPUT.KeyPress(KEY(m))){
+	if (INPUT.key_pressed(KEY(m))){
 		 SETTINGS.showCollision = !SETTINGS.showCollision;
 	}
 
@@ -124,7 +124,7 @@ void StateStage::Update(float time) {
 */
 
 void StateStage::render() {
-	level.background.render(0, 0);
+	level.background_sprite.render(0, 0);
 	// floresta_bg1.render(RENDERPOS(Vec2(0,0)));
 	// floresta_bg2.render(RENDERPOS(Vec2(4433,0)));
 	// floresta_bg3.render(RENDERPOS(Vec2(8465,0)));
@@ -145,7 +145,7 @@ void StateStage::render() {
 	@return The execution of this method returns no value
 */
 void StateStage::Pause() {
-	Camera::Unfollow();
+	Camera::unfollow();
 }
 
 /*!
@@ -154,7 +154,7 @@ void StateStage::Pause() {
 	@return The execution of this method returns no value
 */
 void StateStage::Resume() {
-	Camera::Follow(PLAYER_UID);
+	Camera::follow(PLAYER_UID);
 }
 
 /*!
