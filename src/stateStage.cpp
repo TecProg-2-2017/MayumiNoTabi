0bbf411893c92919b6cca6dc831133d566e33439
Arthur Assis <arthur120496@gmail.com>
2017-11-20 09:18:49 -0200

    applies magic numbers stateStage

--- src/stateStage.cpp
+++ src/stateStage.cpp
@@ -21,6 +21,22 @@
 
 */
 
+#define PLAYER_POSITION_AXIS_X 550.0f
+#define PLAYER_POSITION_AXIS_Y 430.0f
+#define BOAR_POSITION_AXIS_X 2000.0f
+#define BOAR_POSITION_AXIS_Y -100.0f
+#define MIKE_POSITION_AXIS_X 850.0f
+#define MIKE_POSITION_AXIS_Y -100.0f
+#define BANSHEE_START_POSITION_AXIS_x 850.0f
+#define BANSHEE_END_POSITION_AXIS_x 230.0f
+#define BANSHEE_START_POSITION_AXIS_Y -100.0f
+#define BANSHEE_END_POSITION_AXIS_Y -200.0f
+#define MASK_POSITION_AXIS_X 850.0f
+#define MASK_POSITION_AXIS_Y -100.0f
+#define FLOREST_BACKGROUND_SIZE 4032
+
+
+
 //! A constructor.
     /*!
     *	This is a constructor method of StateStage
@@ -29,8 +45,8 @@
 		*/
 StateStage::StateStage(string lvl):levelName{lvl},
 	// floresta_bg1{"img/jogotela-campo-1-away.png"},
-	floresta_bg2{"img/floresta-away.png"},
-	floresta_bg3{"img/jogotela-ponte-away.png"}
+	florest_bg2{"img/floresta-away.png"},
+	florest_bg3{"img/jogotela-ponte-away.png"}
 	{
 	LOG_METHOD_START("StateStage::StateStage");
 	LoadAssets();
@@ -55,7 +71,8 @@ void StateStage::Begin() {
 	LOG_METHOD_START("StateStage::Begin");
 	//! Loads the level and the player
 	level.load_level_from_file(levelName);
-	player = GameObject::MakePlayer(Vec2{550.0f,430.0f});
+	player = GameObject::MakePlayer(Vec2{PLAYER_POSITION_AXIS_X,
+																				PLAYER_POSITION_AXIS_Y});
 	AddObject(player);
 	Camera::follow(player);
 	level.load_level_objects();
@@ -95,7 +112,8 @@ void StateStage::update(float time) {
 
 	//! Checks if the player pressed the key 'u'
 	if (INPUT.key_pressed(KEY(u))){
-		AddObject(GameObject::MakePorco(pos+Vec2{2000.0f,-100.0f}));
+		AddObject(GameObject::MakePorco(pos+Vec2{BOAR_POSITION_AXIS_X,
+																							BOAR_POSITION_AXIS_Y}));
 	}
 	else{
 		// do nothing
@@ -103,7 +121,8 @@ void StateStage::update(float time) {
 
 	//! Checks if the player pressed the key 'p'
 	if (INPUT.key_pressed(KEY(p))){
-		AddObject(GameObject::MakeMike(pos+Vec2{850.0f,-100.0f}));
+		AddObject(GameObject::MakeMike(pos+Vec2{MIKE_POSITION_AXIS_X,
+																						MIKE_POSITION_AXIS_Y}));
 	}
 	else{
 		// do nothing
@@ -111,7 +130,10 @@ void StateStage::update(float time) {
 
 	//! Checks if the player pressed the key 'o'
 	if (INPUT.key_pressed(KEY(o))){
-		AddObject(GameObject::MakeBanshee(pos+Vec2{850.0f,-100.0f},pos+Vec2{230.0f,-200.0f}));
+		AddObject(GameObject::MakeBanshee(pos+Vec2{BANSHEE_START_POSITION_AXIS_x,
+																								BANSHEE_START_POSITION_AXIS_Y},
+																			pos+Vec2{BANSHEE_END_POSITION_AXIS_x,
+																								BANSHEE_END_POSITION_AXIS_Y}));
 	}
 	else{
 		// do nothing
@@ -119,7 +141,8 @@ void StateStage::update(float time) {
 
 	//! Checks if the player pressed the key 'i'
 	if (INPUT.key_pressed(KEY(i))){
-		AddObject(GameObject::MakeMask(pos+Vec2{850.0f,-100.0f}));
+		AddObject(GameObject::MakeMask(pos+Vec2{MASK_POSITION_AXIS_X,
+																						MASK_POSITION_AXIS_Y}));
 	}
 	else{
 		// do nothing
@@ -168,13 +191,13 @@ void StateStage::render() {
 	// floresta_bg1.render(RENDERPOS(Vec2(0,0)));
 	// floresta_bg2.render(RENDERPOS(Vec2(4433,0)));
 	// floresta_bg3.render(RENDERPOS(Vec2(8465,0)));
-	floresta_bg2.render(RENDERPOS(Vec2(0,0)));
-	floresta_bg2.render(RENDERPOS(Vec2(4032*1,0)));
-	floresta_bg2.render(RENDERPOS(Vec2(4032*2,0)));
-	floresta_bg2.render(RENDERPOS(Vec2(4032*3,0)));
-	floresta_bg2.render(RENDERPOS(Vec2(4032*4,0)));
-	floresta_bg2.render(RENDERPOS(Vec2(4032*5,0)));
-	floresta_bg3.render(RENDERPOS(Vec2(4032*6,0)));
+	florest_bg2.render(RENDERPOS(Vec2(0,0)));
+	florest_bg2.render(RENDERPOS(Vec2(FLOREST_BACKGROUND_SIZE*1,0)));
+	florest_bg2.render(RENDERPOS(Vec2(FLOREST_BACKGROUND_SIZE*2,0)));
+	florest_bg2.render(RENDERPOS(Vec2(FLOREST_BACKGROUND_SIZE*3,0)));
+	florest_bg2.render(RENDERPOS(Vec2(FLOREST_BACKGROUND_SIZE*4,0)));
+	florest_bg2.render(RENDERPOS(Vec2(FLOREST_BACKGROUND_SIZE*5,0)));
+	florest_bg3.render(RENDERPOS(Vec2(FLOREST_BACKGROUND_SIZE*6,0)));
 	level.level_tile_map.render();
 	RenderArray();
 	LOG_METHOD_CLOSE("StateStage::render","void");
